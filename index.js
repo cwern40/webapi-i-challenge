5417d0135f85b9a366277f17db12b34e1a55eb66
cwern40 <chris.wernli@hotmail.com>
2019-08-07 19:16:26 -0600

    I added the ability to edit a user and added the message if the user doesn't have a name or bio.

--- index.js
+++ index.js
@@ -48,31 +48,26 @@ server.get('/api/users/:id', (req, res) => {
 server.post('/api/users', (req, res) => {
     const newUser = req.body;
     console.log("new user", newUser.bio)
+
+    if (!newUser.bio || !newUser.name) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user.'
+        })
+    } else {
     db.insert(newUser)
         .then(user => {
-            if (!newUser.bio) {
-                res.status(400).json({
-                    message: 'Please provide name and bio for the user.'
-                })
-            } else {
-                res.status(201).json(user)
-            }
+            res.status(201).json(user)
         })
         .catch(err => {
-            if(!newUser.name) {
-                res.status(400).json({
-                    message: 'Please provide name and bio for the user.'
-                })
-            } else {
             res.status(500).json({
                 err: err,
                 message: 'There was an error while saving the user to the database'
                 })
-            };
-        });
+            });
+    }
 })
 
-//Delet request to delet a user.
+//Delete request to delete a user.
 server.delete('/api/users/:id', (req, res) => {
     const { id } = req.params;
 
@@ -92,6 +87,35 @@ server.delete('/api/users/:id', (req, res) => {
                 message: 'The user could not be removed'
             })
         })
+})
+
+//Put request to update a user.
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    if (!changes.name || !changes.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user.'
+        })
+    } else {
+    db.update(id, changes)
+        .then(updated => {
+            if (updated) {
+                res.json(updated)
+            } else {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist.'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                err: err,
+                message: 'The user information could not be modified.'
+            })
+        })
+    }
 })
 
 server.listen(4000, () => {
